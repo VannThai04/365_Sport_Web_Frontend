20af255454ac931cd348ec2043d42a12e0b6e321
davitofficial43-ai <davitofficial43@gmail.com>
2026-08-26 16:32:25 +0700

    navbar

---
+++ app/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-between items-center ms-6 me-6">
+        <div><img class="w-15 cursor-pointer"
+            src="https://361sport.com/cdn/shop/files/361_logo-BLACK_cee7a490-836a-429f-b257-f2c209a0327a.png?v=1707293000&width=110" alt="">
+        </div>
+        <div class="">
+            <ul class="flex gap-15 mt-2">
+                <li class="hover:text-amber-300"><NuxtLink href="#">Home</NuxtLink></li>
+                <li class="hover:text-amber-300"><NuxtLink href="#">About</NuxtLink></li>
+                <li class="hover:text-amber-300"><NuxtLink href="#">Product</NuxtLink></li>
+                <li class="hover:text-amber-300"><NuxtLink href="#">Contect</NuxtLink></li>
+                <li class="hover:text-amber-300"><NuxtLink href="#">Order</NuxtLink></li>
+            </ul>
+        </div>
+        <div class="flex gap-4">
+            <img class="w-6 mt-3" src="https://img.icons8.com/?size=100&id=132&format=png&color=000000" alt="">
+            <img class="w-6 mt-3" src="https://img.icons8.com/?size=100&id=12438&format=png&color=000000" alt="">
+            <img class="w-6 mt-3" src="https://img.icons8.com/?size=100&id=3686&format=png&color=000000" alt="">
+        </div>
+    </div>
+</template>
